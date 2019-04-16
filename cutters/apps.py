712768204d2e70ce1b4892621ae64f8208bce0b5
schimann <schimannhansen@gmail.com>
2019-04-16 18:55:27 +1200

    my first django try

---
+++ cutters/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CuttersConfig(AppConfig):
+    name = 'cutters'
---
+++ cutters/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.conf import settings
+
+class Cutter(models.Model):
+    cutter_name = models.CharField(max_length=50, primary_key=True)
+    job_no = models.IntegerField(default=0)
+    cutter_id = models.IntegerField(default=0)
+    cutter_in_draw = models.IntegerField(default=0)
+    add_cutter =  models.IntegerField(default=0)
+    sub_cutter = models.IntegerField(default=0)
+
+
+def __str__(self):
+    return self.cutter_name
