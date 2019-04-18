748a8327915e58e3f28c5e066a80cee940a2bdf5
schimann <schimannhansen@gmail.com>
2019-04-18 22:33:57 +1200

    views and QuerySet added

--- cutters/views.py
+++ cutters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Cutter
 
 
 def cutter_list(request):
-    return render(request, 'cutters/cutter_list.html', {})
-    
+    tools = Cutter.objects.filter(cutter_name__icontains = 'hog')
+    return render(request, 'cutters/cutter_list.html', {'tools': tools})
